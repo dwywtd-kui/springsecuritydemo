f1f40aa173705fc2fd7483a63feb3493855b72f3
unknown <1432076549@qq.com>
2021-05-16 17:29:26 +0800

    添加 quit-start

---
+++ demo1-quitstart/src/main/java/com/hanliukui/demo1quitstart/Demo1QuitstartApplication.java
@@ -0,0 +1,13 @@
+package com.hanliukui.demo1quitstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1QuitstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1QuitstartApplication.class, args);
+    }
+
+}
